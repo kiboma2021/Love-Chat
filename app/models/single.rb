786dbfa0137859f2cbfa2849757207e6af83655b
kiboma <omayioben@gmail.com>
2023-07-24 15:24:57 +0300

    Validate singles user input

--- app/models/single.rb
+++ app/models/single.rb
@@ -1,2 +1,5 @@
 class Single < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :bio, presence: true, length: { minimum: 10 }
+    validates :age, presence: true
 end

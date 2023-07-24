bbedcdfc42facede2c0b06cbe3f3706066b439a6
kiboma <omayioben@gmail.com>
2023-07-24 16:16:25 +0300

    Update comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_115950) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_123540) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "single_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "singles", force: :cascade do |t|
     t.string "name"
     t.text "bio"

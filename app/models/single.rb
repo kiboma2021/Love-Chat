fae27ca67202e2692e826adc32a29ac98ae3ed83
kiboma <omayioben@gmail.com>
2023-07-25 07:12:24 +0300

    Add relationship

--- app/models/single.rb
+++ app/models/single.rb
@@ -1,4 +1,5 @@
 class Single < ApplicationRecord
+    has_many :comments, dependent: :destroy
     validates :name, presence: true, uniqueness: true
     validates :bio, presence: true, length: { minimum: 10 }
     validates :age, presence: true

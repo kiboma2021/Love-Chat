5ebaa555196662e69ae9645767a4322ce56ff275
kiboma <omayioben@gmail.com>
2023-07-26 09:01:26 +0300

    Add validation for photos

--- app/models/single.rb
+++ app/models/single.rb
@@ -1,6 +1,7 @@
 class Single < ApplicationRecord
     has_many :comments, dependent: :destroy
+    has_one_attached :photo
     validates :name, presence: true, uniqueness: true
     validates :bio, presence: true, length: { minimum: 10 }
-    validates :age, presence: true
+    validates_presence_of :age, :photo
 end
